be8b74ea8d90666533dc7cf5944a0fbe04a43b8d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-25 03:30:30 +0100

    added a comment

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ var timer = setInterval(function(){
     time += 2;
     console.log(time + ' seconds have passed...');
     if(time > 5){
-        clearInterval();
+        clearInterval(timer);
     }
 }, 2000);
