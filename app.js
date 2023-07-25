31c72227455ae9500d7522cb9d03bb18592dee79
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-25 03:29:02 +0100

    added clear interval

--- app.js
+++ app.js
@@ -21,4 +21,7 @@ var time = 0;
 var timer = setInterval(function(){
     time += 2;
     console.log(time + ' seconds have passed...');
+    if(time > 5){
+        clearInterval();
+    }
 }, 2000);
