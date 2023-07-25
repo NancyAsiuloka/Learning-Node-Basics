e5c55f5b869da35eadd6c5ee5b15cd5b09adb73b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-25 03:25:05 +0100

    added a set interval function

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write('Hello World!');
-        res.end();
-    }
-    if (req.url === '/api/courses'){
-        res.write(JSON.stringify([1, 2, 3]));
-        res.end();
-    }
-});
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.write('Hello World!');
+//         res.end();
+//     }
+//     if (req.url === '/api/courses'){
+//         res.write(JSON.stringify([1, 2, 3]));
+//         res.end();
+//     }
+// });
 
-server.listen(3000);
+// server.listen(3000);
 
-console.log('Listening on port 3000...');
+// console.log('Listening on port 3000...');
+
+var time = 0;
+
+setInterval(function(){
+    tme += 2;
+    console.log(time + ' seconds have passed...');
+}, 2000);
