c10cee2df458d0dda1bb47eea4beac0db9983fa8
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-26 06:00:05 +0100

    added a comment

--- logger.js
+++ logger.js
@@ -2,14 +2,14 @@ var counter = function(arr){
     return 'there are ' + arr.length + ' elements in this array';
 };
 
-var adder = function(a, b){
-    return `The sum of the 2 numbers is ${a+b}`;
-};
+// var adder = function(a, b){
+//     return `The sum of the 2 numbers is ${a+b}`;
+// };
 
-var pi = 3.146;
+// var pi = 3.146;
 
-module.exports = {
-    counter: counter,
-    adder: adder,
-    pi: pi
-};
+// module.exports = {
+//     counter: counter,
+//     adder: adder,
+//     pi: pi
+// };
