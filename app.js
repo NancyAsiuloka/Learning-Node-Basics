c9924400c5e7196efc17be5a965180e39125a61d
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-28 05:26:28 +0100

    added a comment

--- app.js
+++ app.js
@@ -173,7 +173,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url)
+    console.log('request was made: ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hey Ninjas');
 });
