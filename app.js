26fb55fa6ea7777f87560f623c053c0670ffff6b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-29 13:45:21 +0100

    added a comment

--- app.js
+++ app.js
@@ -170,16 +170,16 @@
 
 // CLIENTS & SERVERS
 // Creating a Server
-var http = require('http');
+// var http = require('http');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hey Ninjas');
-});
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hey Ninjas');
+// });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+// server.listen(3000, '127.0.0.1');
+// console.log('yo dawgs, now listening to port 3000');
 
 
 
