f63843a94d498fd9a85d43add90c0b3fb946b441
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-29 14:01:45 +0100

    added a event emitter property

--- app.js
+++ app.js
@@ -186,7 +186,10 @@ var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt');
 
-
+myReadStream.on('data', function(chunk) {
+    console.log('new chunk received:');
+    console.log(chunk);
+});
 
 
 
