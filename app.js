2f573596844401903b49ce437a10f7436793e063
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-30 10:33:07 +0100

    added a comment

--- app.js
+++ app.js
@@ -170,26 +170,46 @@
 
 // CLIENTS & SERVERS
 // Creating a Server
-// var http = require('http');
-
-// var server = http.createServer(function(req, res){
-//     console.log('request was made: ' + req.url);
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hey Ninjas');
-// });
-
-// server.listen(3000, '127.0.0.1');
-// console.log('yo dawgs, now listening to port 3000');
-
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt');
-
-myReadStream.on('data', function(chunk) {
-    console.log('new chunk received:');
-    console.log(chunk);
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    myReadStream.pipe(res);
 });
+
+server.listen(3000, '127.0.0.1');
+console.log('yo dawgs, now listening to port 3000');
+
+// HOW TO CREATE A READABLE STREAM
+// var http = require('http');
+// var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+// myReadStream.on('data', function(chunk) {
+//     console.log('new chunk received:');
+//     console.log(chunk);
+// });
+
+// HOW TO CREATE A WRITABLE STREAM
+// var http = require('http');
+// var fs = require('fs');
+
+// var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+// var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+// myReadStream.on('data', function(chunk) {
+//     console.log('new chunk received:');
+//     myWriteStream.write(chunk);
+// });
+// the easier way to do the above function using PIPE
+// myReadStream.pipe(myWriteStream);
+
+
+
 
 
 
