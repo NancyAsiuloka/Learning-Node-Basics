23d7704eb7a2828abea59344f0eed681eb96787c
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-30 10:34:00 +0100

    added a comment

--- app.js
+++ app.js
@@ -170,18 +170,18 @@
 
 // CLIENTS & SERVERS
 // Creating a Server
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-    myReadStream.pipe(res);
-});
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+//     myReadStream.pipe(res);
+// });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+// server.listen(3000, '127.0.0.1');
+// console.log('yo dawgs, now listening to port 3000');
 
 // HOW TO CREATE A READABLE STREAM
 // var http = require('http');
