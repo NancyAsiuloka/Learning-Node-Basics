f4034459233c288a345fedd862395f0a0f0e98f5
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-07-31 15:45:41 +0100

    created a server

--- app.js
+++ app.js
@@ -210,21 +210,73 @@
 
 // SERVING HTML PAGES
 
+// var http = require('http');
+// var fs = require('fs');
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+//     myReadStream.pipe(res);
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('yo dawgs, now listening to port 3000');
+
+// SERVING JSON TO THE CLIENTS
+// var http = require('http');
+// var fs = require('fs');
+
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     var myObj = {
+//         name: 'Nancy',
+//         job: 'Backend Engineer',
+//         age: 23
+//     };
+//     res.end(JSON.stringify(myObj));
+// });
+
+// server.listen(3000, '127.0.0.1');
+// console.log('yo dawgs, now listening to port 3000');
+
+// BASIC ROUTING
 var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-    myReadStream.pipe(res);
+    if(req.url === '/home' || req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if(req.url === '/api/ninjas'){
+        var ninjas = [{
+            name: 'Nancy',
+            age: 23,
+        },
+        {
+            name: 'Gideon',
+            age: 24,
+        }
+    ];
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+
+    }
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('yo dawgs, now listening to port 3000');
 
-
-
+// THE NODE PACKAGE MANAGER (npm)
+// PACKAGE.JSON FILE
 
 
 
