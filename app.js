50c56ae35cb834f831037c70562f1dd56b63a898
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-08-01 18:49:48 +0100

    added route params

--- app.js
+++ app.js
@@ -242,42 +242,77 @@
 // console.log('yo dawgs, now listening to port 3000');
 
 // BASIC ROUTING
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-var server = http.createServer(function(req, res){
-    console.log('request was made: ' + req.url);
-    if(req.url === '/home' || req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/index.html').pipe(res);
-    } else if (req.url === '/contact'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/contact.html').pipe(res);
-    } else if(req.url === '/api/ninjas'){
-        var ninjas = [{
-            name: 'Nancy',
-            age: 23,
-        },
-        {
-            name: 'Gideon',
-            age: 24,
-        }
-    ];
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(ninjas));
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream(__dirname + '/404.html').pipe(res);
+// var server = http.createServer(function(req, res){
+//     console.log('request was made: ' + req.url);
+//     if(req.url === '/home' || req.url === '/') {
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/index.html').pipe(res);
+//     } else if (req.url === '/contact-us'){
+//         res.writeHead(200, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/contact.html').pipe(res);
+//     } else if(req.url === '/api/ninjas'){
+//         var ninjas = [{
+//             name: 'Nancy',
+//             age: 23,
+//         },
+//         {
+//             name: 'Gideon',
+//             age: 24,
+//         }
+//     ];
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     res.end(JSON.stringify(ninjas));
+//     } else {
+//         res.writeHead(404, {'Content-Type': 'text/html'});
+//         fs.createReadStream(__dirname + '/404.html').pipe(res);
 
-    }
-});
+//     }
+// });
 
-server.listen(3000, '127.0.0.1');
-console.log('yo dawgs, now listening to port 3000');
+// server.listen(3000, '127.0.0.1');
+// console.log('yoo!, now listening to port 3000');
 
 // THE NODE PACKAGE MANAGER (npm)
 // PACKAGE.JSON FILE
 // INSTALLING NODEMON
+// INTRODUCTION TO EXPRESS
+// var express = require('express');
+
+// var app = express();
+
+// app.get('/', function (req, res) {
+//     res.send('this is the homepage');
+// });
+
+// app.get('/contact', function (req, res) {
+//     res.send('this is the contact page');
+// });
+
+// app.listen(3000);
+
+// ROUTE PARAMETERS
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('this is the homepage');
+});
+
+app.get('/contact', function (req, res) {
+    res.send('this is the contact page');
+});
+
+app.get('/profile/:name', function (req, res) {
+    res.send('You requested to see a profile with the id of ' + req.params.name);
+})
+
+app.listen(3000);
+
 
 
 
